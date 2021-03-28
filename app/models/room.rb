d20181041e732ea8f036e158037fa73c535b54e7
yasuyasu-ot <y.otsubo.dss@gmail.com>
2021-03-28 18:15:23 +0900

    Roomの新規登録・編集時の会議室名、場所、収容人数を必須項目とする実装

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,2 +1,8 @@
 class Room < ApplicationRecord
+  validates :name, :place, :number, presence: true
+
+  # validates :name, length: { minimum: 30, minimum: 1 }
+  # validates :place, inclusion: { in: %w[東京 大阪 金沢 福岡 札幌 仙台 名古屋 金沢] }
+  # validates :number, numericality: { greater_than: 5, less_than: 30 }
+  # validates :name, format: { with: /.+\d{2}/ }
 end
