0e96dc9cc43cd66ebf0f67cf9f207f5b17b3e173
yasuyasu-ot <y.otsubo.dss@gmail.com>
2021-03-29 01:09:49 +0900

    収容人数のチェック

--- app/models/room.rb
+++ app/models/room.rb
@@ -4,4 +4,11 @@ class Room < ApplicationRecord
   validates :place, inclusion: { in: %w[東京 大阪 金沢 福岡 札幌 仙台 名古屋 金沢] }
   validates :number, numericality: { greater_than_or_equal_to: 5, less_than_or_equal_to: 30 }
   validates :name, format: { with: /\A.+#\d{2}\z/, message: '会議室名が正しくありません' }
+  validate :number_5multiple_check
+
+  private
+
+  def number_5multiple_check
+    errors.add(:number, '収容人数は５の倍数で指定してください') unless number % 5 == 0
+  end
 end
