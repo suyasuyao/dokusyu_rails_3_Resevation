1d8ada359e039a556b222efca9c9e3f2531204fc
yasuyasu-ot <y.otsubo.dss@gmail.com>
2021-03-29 02:51:52 +0900

    entryモデルにスコープ追加

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,2 +1,8 @@
 class Entry < ApplicationRecord
+  scope :least_entries, lambda { |scope_date|
+                          where('reserved_date <= ? + 7 ', scope_date).where('reserved_date >= ? - 7 ', scope_date)
+                        }
+  # Ex:- scope :active, -> {where(:active => true)}
 end
+
+# &&  reserved_date >= ? - 7    scope_date
