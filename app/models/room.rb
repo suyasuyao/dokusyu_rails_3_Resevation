1157fba822c0650446f325e42e151678f472015e
yasuyasu-ot <y.otsubo.dss@gmail.com>
2021-03-28 22:59:40 +0900

    Room名の形式に一致しない場合のメッセージ変更

--- app/models/room.rb
+++ app/models/room.rb
@@ -3,5 +3,5 @@ class Room < ApplicationRecord
   validates :name, length: { maximum: 30, minimum: 1 }
   validates :place, inclusion: { in: %w[東京 大阪 金沢 福岡 札幌 仙台 名古屋 金沢] }
   validates :number, numericality: { greater_than_or_equal_to: 5, less_than_or_equal_to: 30 }
-  validates :name, format: { with: /\A.+#\d{2}\z/ }
+  validates :name, format: { with: /\A.+#\d{2}\z/, message: '会議室名が正しくありません' }
 end
